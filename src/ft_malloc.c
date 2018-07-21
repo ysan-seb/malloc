a4278765f7cde53429ae0e28dffecb2a2e3e6337
Yann SAN SEBASTIAN <ysan-seb@e1r4p13.42.fr>
2018-07-21 16:11:10 +0200

    ft'

--- src/ft_malloc.c
+++ src/ft_malloc.c
@@ -6,7 +6,7 @@
 /*   By: ysan-seb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/14 10:34:28 by ysan-seb          #+#    #+#             */
-/*   Updated: 2018/07/14 16:58:10 by ysan-seb         ###   ########.fr       */
+/*   Updated: 2018/07/21 16:10:46 by ysan-seb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,88 +17,97 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct      s_zone
+typedef struct		s_zone
 {
-    size_t          size;
-    int             free;
-    struct s_zone   *next;
-}                   t_zone;
+	size_t			size;
+	struct s_zone	*next;
+}					t_zone;
 
-typedef struct      s_tsl
+typedef struct		s_tsl
 {
-    t_zone          *lzone;
-}                   t_tsl;
+	t_zone			*lzone;
+}					t_tsl;
 
-t_tsl              g_tsl;
+t_tsl				g_tsl;
 
-void                *ft_malloc(size_t size)
+void				*ft_malloc(size_t size)
 {
-    int i = 0;
-	void *ptr;
-    t_tsl tsl = g_tsl;
-	size_t len;
+	void	*ptr;
+	t_tsl	tsl;
+	size_t	len;
 
-
-	if (size == 0)
-		return (NULL);	
-	len = (size - 1) + sizeof(t_zone) / getpagesize() + 1;
-    if ((ptr = mmap(NULL, len * getpagesize(), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED)
-        return (NULL);
-    printf("ptr tsl = %p\n", tsl.zone);
-    if (!tsl.lzone) {
-        tsl.lzone = ptr;
-        printf("tsl = %p\n", tsl.lzone);    
-        tsl.lzone->size = size;
-        tsl.lzone->free = 1;
-        tsl.lzone->next = NULL;
-        printf("tsl = %p\n", ptr);
-    } else {
-        while (tsl.lzone->next)
-        {
-            tsl.lzone = tsl.lzone->next;
-            i++;
-        }
-        printf("%d\n", i);
-        tsl.lzone = ptr;
-        printf("ptr = %p\n", ptr);
-        printf("new ptr = %p\n", tsl.lzone);
-        printf("new ptr + 24 = %p\n", tsl.lzone + sizeof(t_tsl));
-    }
-	return (tsl.lzone + sizeof(t_tsl));
+	tsl = g_tsl;
+	if (size > 0)
+	{
+		len = ((size - 1) + sizeof(t_tsl)) / getpagesize() + 1;
+		if (!g_tsl.lzone)
+		{
+			if ((g_tsl.lzone = mmap(NULL, len * getpagesize(), PROT_READ
+			| PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED)
+				return (NULL);
+			g_tsl.lzone->size = len;
+			return (g_tsl.lzone + sizeof(t_tsl));
+		}
+		else
+		{
+			if ((ptr = mmap(NULL, len * getpagesize(), PROT_READ
+			| PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED)
+				return (NULL);
+			while (tsl.lzone->next)
+				tsl.lzone = tsl.lzone->next;
+			tsl.lzone->next = ptr;
+			tsl.lzone = tsl.lzone->next;
+			tsl.lzone->size = len;
+			tsl.lzone->next = NULL;
+			return (tsl.lzone + sizeof(t_tsl));
+		}
+	}
+	return (NULL);
 }
 
-    // printf("ptr = %p\n", ptr);
-    // printf("zone = %p\n", g_tsl.lzone);
-    // printf("zone  + sizeof t_tsl = %p\n", g_tsl.lzone + sizeof(t_tsl));
-    // printf("sizeof struct = %zu\n", sizeof(size_t));
-
-void                ft_free(void *ptr)
+void				ft_free(void *ptr)
 {
-    int ret;
+	int	ret;
 
-    // ret = munmap(ptr, size * getpagesize());
-    printf("ret = %d\n", ret);
+	// ret = munmap(ptr, size * getpagesize());
+	printf("ret = %d\n", ret);
 }
 
-int main(int ac, char **av)
+void				ft_show_alloc_mem(void)
 {
-    int i;
-    char *ptr1;
+	int		i;
+	t_tsl	tsl;
 
-    i = 0;
-    if (ac < 2)
-        return (-1);
-    else
-    {
-        ptr1 = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
-        printf("str1 = %p\n", ptr1);
-        ptr1 = strcpy(ptr1, av[1]);
-        ptr1 = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
-        ptr1 = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
-        ptr1 = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
-        ptr1 = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
-        printf("str2 = %p\n", ptr1);
-        ft_free(ptr1);
-    }
-    return (0);
+	i = 0;
+	tsl = g_tsl;
+	printf("\n");
+	while (tsl.lzone->next)
+	{
+		printf("zone[%d]\nAdress : %p\nPtr : %p\nSize : %zu\n\n",
+				i, tsl.lzone, tsl.lzone + sizeof(t_tsl), tsl.lzone->size);
+		tsl.lzone = tsl.lzone->next;
+		i++;
+	}
+	printf("zone[%d]\nAdress : %p\nPtr : %p\nSize : %zu\n",
+			i, tsl.lzone, tsl.lzone + sizeof(t_tsl), tsl.lzone->size);
+}
+
+int					main(int ac, char **av)
+{
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	if (ac < 2)
+		return (-1);
+	else
+	{
+		ptr = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
+		ptr = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
+		ptr = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
+		ptr = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
+		ptr = (char *)ft_malloc(sizeof(char) * strlen(av[1]));
+		ft_show_alloc_mem();
+	}
+	return (0);
 }
